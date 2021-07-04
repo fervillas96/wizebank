630d04c262614d669508c29417a91be9f82bae2f
Fer Villanueva <jorge.villanueva@wizeline.com>
2021-07-04 13:16:17 -0500

    feat: new paths configuration

--- pages/bankAccount/index.jsx
+++ pages/bankAccount/index.jsx
@@ -1,7 +1,7 @@
-import MobileAppContainer from '../../components/MobileAppContainer';
-import NavBar from '../../components/NavBar';
-import BodyContainer from '../../components/BodyContainer';
-import NavigationButtons from '../../components/NavigationButtons';
+import MobileAppContainer from '@components/MobileAppContainer';
+import NavBar from '@components/NavBar';
+import BodyContainer from '@components/BodyContainer';
+import NavigationButtons from '@components/NavigationButtons';
 
 const BankAccount = () => {
   return (  
